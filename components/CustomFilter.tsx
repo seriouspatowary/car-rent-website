a35fb3d11c1797483e2834a888a6954eb882928d
seriouspatowary <simantapatowary963@gmail.com>
2024-01-24 20:05:32 +0530

    first commit

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from 'react'
+
+import { customFilterProps } from '@/types';
+
+const CustomFilter = ({title}:customFilterProps) => {
+   
+  return (
+    <form  >
+     CustomFilter
+    </form>
+    
+  )
+}
+
+export default CustomFilter
---
+++ types/index.ts
@@ -0,0 +1,35 @@
+import { MouseEventHandler } from "react";
+
+export interface customButtonProps{
+    title:string;
+    containerStyles?: string;
+    handleClick?:MouseEventHandler<HTMLButtonElement>;
+    btnType?:"button" | "submit";
+    textStyles?:string,
+    rightIcon?: string,
+    isDisabled?: boolean
+}
+export interface customFilterProps{
+    title:string;
+    
+}
+export interface SearchManufacturerProps{
+    manufacturer:string;
+    setManufacturer:(manufacturer:string) => void;
+    
+}
+export interface CarProps{
+city_mpg:number,
+class:string,
+combination_mpg:number,
+cylinders:number,
+displacement:number,
+drive:string,
+fuel_type:string,
+highway_mpg:number,
+make:string,
+model:string,
+transmission:string,
+year:number
+    
+}
